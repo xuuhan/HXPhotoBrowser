3211b2c375ab0c837da02d076ec7f36472066c38
hanxu <h2032932@163.com>
2018-08-12 23:34:07 +0800

    开始

---
+++ HXPhotoBrowser/HXBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  HXBrowserViewController.h
+//  HXPhotoBrowser
+//
+//  Created by suin on 2018/8/12.
+//  Copyright © 2018年 韩旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXBrowserViewController : UIViewController
+
+@end
--- HXPhotoBrowser/ViewController.m
+++ HXPhotoBrowser/ViewController.m
@@ -22,7 +22,7 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
