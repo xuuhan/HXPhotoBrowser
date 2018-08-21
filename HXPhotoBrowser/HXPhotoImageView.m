0db5002462464615bcbd7516844de7181aedf8b1
hanxu <h2032932@163.com>
2018-08-21 19:19:48 +0800

    shadeView

--- HXPhotoBrowser/HXPhotoImageView.m
+++ HXPhotoBrowser/HXPhotoImageView.m
@@ -7,9 +7,26 @@
 //
 
 #import "HXPhotoImageView.h"
+#import "HXShadeView.h"
+
+@interface HXPhotoImageView()
+@property (nonatomic, strong) HXShadeView *shade;
+@end
 
 @implementation HXPhotoImageView
 
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self setShade];
+    }
+    return self;
+}
 
+- (void)setShade{
+    _shade = [[HXShadeView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+    [self addSubview:_shade];
+    _shade.backgroundColor = [UIColor blackColor];
+    _shade.alpha = 0.5;
+}
 
 @end
---
+++ HXPhotoBrowser/HXShadeView.m
@@ -0,0 +1,24 @@
+//
+//  HXShadeView.m
+//  HXPhotoBrowser
+//
+//  Created by suin on 2018/8/21.
+//  Copyright © 2018年 韩旭. All rights reserved.
+//
+
+#import "HXShadeView.h"
+
+@implementation HXShadeView
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if(self = [super initWithFrame:frame]){
+        
+    }
+    return self;
+}
+
+- (void)setUI{
+    
+}
+
+@end
